f1ad2688648ece59bf054c188d9f7a838050610f
Patryk <byszek.patryk@gmail.com>
2022-05-14 23:56:34 +0200

    Work on layout

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,15 +1,23 @@
 import { Box, Container } from "@chakra-ui/react"
 import Head from "next/head"
 
+import Navbar from "../navbar"
+
 const Layout = ({ children, router }) => {
   return (
-    <Box as="main">
+    <Box as="main" h="calc(100vh)">
       <Head>
-        <meta name="description" content="Patryk's portfolio :)" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Patryk's portfolio" />
         <meta name="author" content="Patryk Byszek" />
         <title>Patryk's portfolio</title>
       </Head>
-      <Container maxW={"container.lg"}>{children}</Container>
+      {/* <Container maxW={"container.lg"}> */}
+      <Navbar path={router.asPath} />
+      {/* </Container> */}
+      <Container maxW={"container.lg"} pt={15}>
+        {children}
+      </Container>
     </Box>
   )
 }
