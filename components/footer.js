f2998fec8b80f9a2437fb592615aa7ae419187f1
Patryk <byszek.patryk@gmail.com>
2022-05-15 02:08:46 +0200

    Footer component created

---
+++ components/footer.js
@@ -0,0 +1,22 @@
+import { Box } from "@chakra-ui/react"
+
+const Footer = () => {
+  return (
+    <Box
+      align="center"
+      opacity={0.4}
+      fontSize="sm"
+      pb={4}
+      css={{
+        position: "absolute",
+        left: 0,
+        bottom: 0,
+        right: 0,
+      }}
+    >
+      &copy; {new Date().getFullYear()} Patryk Byszek. All Rights Reserved.
+    </Box>
+  )
+}
+
+export default Footer
