2f9f415d9843827e7b1d2154ec6751eb3b855a28
Patryk <byszek.patryk@gmail.com>
2022-05-18 23:54:39 +0200

    Work on layout

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,22 +1,45 @@
-import { Box, Container } from "@chakra-ui/react"
-import Head from "next/head"
+import { useEffect, useState } from "react"
 
-import Navbar from "../navbar"
+import Head from "next/head"
+import dynamic from "next/dynamic"
+
+import Footer from "../Footer"
+import Navbar from "../Navbar"
+import VoxelLoader from "../VoxelLoader"
+
+import VoxelDogLoader from "../../comp2/voxel-dog-loader"
+
+import { Box, Container } from "@chakra-ui/react"
+
+const DynamicVoxelModel = dynamic(() => import("../VoxelModel"), {
+  ssr: false,
+  loading: () => <VoxelLoader />,
+})
+const LazyVoxelDog = dynamic(() => import("../../comp2/voxel.dog"), {
+  ssr: false,
+  loading: () => <VoxelDogLoader />,
+})
+
+// const dynamicVoxelModel = dynamic(() => import("../VoxelModel"), {
+//   ssr: false,
+//   loading: () => <VoxelLoader />,
+// })
 
 const Layout = ({ children, router }) => {
   return (
-    <Box as="main" h="calc(100vh)">
+    <Box as="main" h="calc(100vh)" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Patryk's portfolio" />
         <meta name="author" content="Patryk Byszek" />
         <title>Patryk's portfolio</title>
       </Head>
-      {/* <Container maxW={"container.lg"}> */}
       <Navbar path={router.asPath} />
-      {/* </Container> */}
-      <Container maxW={"container.lg"} pt={15}>
+      <Container maxW={"container.lg"} pt={14}>
+        <DynamicVoxelModel />
+        {/* <LazyVoxelDog /> */}
         {children}
+        <Footer />
       </Container>
     </Box>
   )
