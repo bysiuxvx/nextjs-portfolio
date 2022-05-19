11f181b9af5fd9168380b3d920b0c9e11bc6470b
Patryk <byszek.patryk@gmail.com>
2022-05-19 23:36:34 +0200

    Initial setup for subpages

---
+++ pages/about/index.js
@@ -0,0 +1,114 @@
+import Head from "next/head"
+import dynamic from "next/dynamic"
+
+import PageWrapper from "../../components/layouts/PageWrapper"
+import Emoji from "../../components/Emoji"
+
+// import Tippy from "@tippy.js/react"
+import { followCursor } from "tippy.js"
+
+import {
+  Container,
+  Box,
+  Center,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react"
+import Paragraph from "../../components/Paragraph"
+import {
+  SvgContainer,
+  SvgWrapper,
+  TechnologiesSection,
+  StyledIcon,
+} from "../../components/TechnologiesContainers"
+
+import "tippy.js/dist/tippy.css"
+import { technologiesList } from "./technologiesList"
+
+const About = () => {
+  const Tooltip = dynamic(() => import("@tippy.js/react"), { ssr: "false" })
+
+  const technologies = technologiesList.map((technology) => (
+    <Tooltip
+      content={technology.comment}
+      followCursor={true}
+      plugins={[followCursor]}
+    >
+      <SvgWrapper key={technology.name}>
+        <StyledIcon as={technology.image} />
+      </SvgWrapper>
+    </Tooltip>
+  ))
+
+  const myAge = () => {
+    var today = new Date()
+    var birthDate = new Date("1991-07-16")
+    var age = today.getFullYear() - birthDate.getFullYear()
+    var m = today.getMonth() - birthDate.getMonth()
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--
+    }
+    return age
+  }
+
+  return (
+    <PageWrapper maxW={"container.md"}>
+      <Center>
+        <Box
+          borderRadius={"lg"}
+          p={3}
+          bg="#e2d6bf80"
+          css={{ backdropFilter: "blur(100px)" }}
+          position={"relative"}
+          textAlign="center"
+          w={"20em"}
+        >
+          Hi again, nice to meet you!
+        </Box>
+      </Center>
+      <Box maxW={"container.sm"}>
+        <Heading as={"h2"} variant="page-title">
+          Patryk Byszek
+          <br />
+          DODAJ TEKST JAKIS
+        </Heading>
+        <Heading as={"p"} variant="section-title" fontSize={"1.5em"}>
+          Bio
+        </Heading>
+        <Paragraph>
+          I'm a junior frontend developer, with close to two years of hobbyist
+          experience. Prior to that I've worked on several technical positions,
+          from local and remote support to IT / System admin. Currently I am
+          unfortunately not working professionally as a developer, but I am
+          still trying to find a spot for me. As of now I am doing my best to
+          become a fullstack dev by learning Node, specifically Express.
+        </Paragraph>
+        <Heading as={"p"} variant="section-title" fontSize={"1.5em"}>
+          Tech
+        </Heading>
+        <Paragraph>
+          Some of the technologies I am familiar with and used during my
+          commercial experience:
+        </Paragraph>
+        <TechnologiesSection>
+          <SvgContainer bg={useColorModeValue("pink", "red")}>
+            {technologies}
+          </SvgContainer>
+        </TechnologiesSection>
+        <Heading as={"p"} fontSize={"1.5em"}>
+          Hobbies
+        </Heading>
+        <Paragraph>
+          My free time I either spend relaxing with my dog, learning something
+          new, or playing guitar. I used to play in a band and even released two
+          records on vinyl. I am also a bit of a gamer. Besides the mentioned, I
+          also am interested in electronics and with the help of tutorials I've
+          built small devices like guitar pedal effects - most of them worked...
+        </Paragraph>
+      </Box>
+    </PageWrapper>
+  )
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,82 @@
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+
 import Head from "next/head"
-import Image from "next/image"
+import Typewriter from "typewriter-effect"
 
-import React from "react"
+import PageWrapper from "../components/layouts/PageWrapper"
 
-const Home = () => {
+import { Box, Container, Heading } from "@chakra-ui/react"
+
+const Page = () => {
+  // const [introPlayed, setIntroPlayed] = useState(false)
+
+  const router = useRouter()
+
   return (
-    <div>
+    <PageWrapper
+      // pt={0}
+      maxW={"container.lg"}
+      mb={30}
+    >
       <Head>
-        <title>Create Next App</title>
         <meta
           name="description"
           content="Personal portfolio of Patryk Byszek."
         />
         <link rel="icon" href="/favicon.ico" />
+        <title>Test</title>
       </Head>
-      <main>
-        <h1>Yo</h1>
-      </main>
-    </div>
+      <Container>
+        <Heading fontSize={["30px", "50px"]} userSelect={"none"}>
+          <Typewriter
+            options={{ cursor: "" }}
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(500)
+                .typeString("Hello World!")
+                .pauseFor(500)
+                .deleteChars("6")
+                .typeString("future employer!")
+                .pauseFor(500)
+                .deleteChars("16")
+                .pauseFor(500)
+                .typeString("friend!")
+                .pauseFor(500)
+                .deleteChars("7")
+                .pauseFor(500)
+                .typeString("mom!")
+                .pauseFor(500)
+                .deleteChars("4")
+                .typeString("World!")
+                .start()
+            }}
+          />
+        </Heading>
+        <Heading
+          fontSize={["25px", "45px"]}
+          fontWeight="normal"
+          userSelect={"none"}
+        >
+          <Typewriter
+            options={{ cursor: "" }}
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(3500)
+                .typeString("I hope you will enjoy what you see!<br/>")
+                .pauseFor(1000)
+                .typeString("All of my projects are stolen")
+                .pauseFor(1000)
+                .deleteChars(10)
+                .pauseFor(1000)
+                .typeString("<strong>were created</strong> by me!")
+                .start()
+            }}
+          />
+        </Heading>
+      </Container>
+    </PageWrapper>
   )
 }
 
-export default Home
+export default Page
