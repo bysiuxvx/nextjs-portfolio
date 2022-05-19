1e780fb252ee845dce0289f2a73448b20cc6c730
Patryk <byszek.patryk@gmail.com>
2022-05-19 23:36:15 +0200

    Initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { ChakraProvider } from "@chakra-ui/react"
+import Chakra from "../components/Chakra"
 
+import Layout from "../components/layouts/Main"
+
+const MyApp = ({ Component, pageProps, router }) => {
+  return (
+    <Chakra>
+      <Layout router={router}>
+        <Component {...pageProps} key={router.route} />
+      </Layout>
+    </Chakra>
+  )
+}
 export default MyApp
