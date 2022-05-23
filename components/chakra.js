e45833af93a0ed7e483360ea6c45a2477b7da53e
Patryk <byszek.patryk@gmail.com>
2022-05-23 20:35:13 +0200

    Chakra provider fix

--- components/chakra.js
+++ components/chakra.js
@@ -3,11 +3,9 @@ import {
   cookieStorageManager,
   localStorageManager,
 } from "@chakra-ui/react"
-
 import theme from "../lib/theme"
 
 export default function Chakra({ cookies, children }) {
-  // b) Pass `colorModeManager` prop
   const colorModeManager =
     typeof cookies === "string"
       ? cookieStorageManager(cookies)
@@ -20,12 +18,9 @@ export default function Chakra({ cookies, children }) {
   )
 }
 
-// also export a reusable function getServerSideProps
 export async function getServerSideProps({ req }) {
   return {
     props: {
-      // first time users will not have any cookies and you may not return
-      // undefined here, hence ?? is necessary
       cookies: req.headers.cookie ?? "",
     },
   }
