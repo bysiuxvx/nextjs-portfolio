8668cc3e4255137a1e129c146c766fd596a5d223
Patryk <byszek.patryk@gmail.com>
2022-05-23 20:21:47 +0200

    Fixing unmet dependency issue

--- components/VoxelLoader.js
+++ components/VoxelLoader.js
@@ -17,7 +17,7 @@ export const VoxelContainer = forwardRef(({ children }, ref) => (
     ref={ref}
     pt={35}
     m="auto"
-    mt={["-20px", "-60px", "-90px"]}
+    mt={["-20px", "-60px", "-120px"]}
     mb={["-40px", "-140px", "-200px"]}
     w={[280, 480, 640]}
     h={[280, 480, 640]}
