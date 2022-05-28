48e887551181b845de7f9cf93fde4a2387fe5f31
Patryk <byszek.patryk@gmail.com>
2022-05-28 23:20:13 +0200

    Work on project section

--- components/projects/ProjectItem.js
+++ components/projects/ProjectItem.js
@@ -1,28 +1,22 @@
 import dynamic from "next/dynamic"
 import Image from "next/image"
 
-import {
-  Box,
-  Heading,
-  Text,
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionPanel,
-  AccordionItem,
-  GridItem,
-} from "@chakra-ui/react"
+import { Box, Heading } from "@chakra-ui/react"
+import { Global } from "@emotion/react"
 
-const Project = ({ project }) => {
-  // const img = dynamic(
-  //   () => (
-  //     <Image src={project.image} alt={project.imageAlt} placeholder="blur" />
-  //   ),
-  //   { ssr: false, loading: () => {} }
-  // )
-
+const ProjectItem = ({ project, setModalContent, onOpen }) => {
   return (
-    <Box w="%" textAlign="center">
+    <Box
+      w={"100%"}
+      textAlign="center"
+      className="project-item-container"
+      cursor={"pointer"}
+      onClick={() => {
+        onOpen
+        setModalContent(project)
+      }}
+      title={project.name}
+    >
       <Image
         width={"720"}
         height={"400"}
@@ -31,10 +25,15 @@ const Project = ({ project }) => {
         placeholder="blur"
         p={5}
         objectFit="contain"
+
         // priority
       />
 
       <Heading fontSize={18}>{project.name}</Heading>
+
+      <ProjectItemStyle />
+
+      {/* <Heading fontSize={18}>{project.name}</Heading>
       <Accordion allowToggle allowMultiple>
         <AccordionItem>
           <Text>
@@ -50,8 +49,24 @@ const Project = ({ project }) => {
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
+      <ProjectItemStyle /> */}
     </Box>
   )
 }
 
-export default Project
+export default ProjectItem
+
+export const ProjectItemStyle = () => (
+  <Global
+    styles={`
+.project-item-container {
+  background: rgba( 255, 255, 255, 0.25 );
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 4px );
+  -webkit-backdrop-filter: blur( 4px );
+  border-radius: 10px;
+  border: 1px solid rgba( 255, 255, 255, 0.18 );
+}
+`}
+  />
+)
--- components/projects/ProjectList.js
+++ components/projects/ProjectList.js
@@ -1,17 +1,33 @@
-import Project from "./ProjectItem"
+import { useState } from "react"
 
 import { projectList } from "../../data/project-list"
 
-import { Container, SimpleGrid } from "@chakra-ui/react"
+import ProjectItem from "./ProjectItem"
+import ProjectModal from "./ProjectModal"
+
+import { Container, SimpleGrid, useDisclosure } from "@chakra-ui/react"
 
 const ProjectList = () => {
+  const [modalContent, setModalContent] = useState()
+
   const projects = projectList.map((project) => (
-    <Project key={project.name} project={project} />
+    <ProjectItem
+      key={project.name}
+      project={project}
+      setModalContent={setModalContent}
+    />
   ))
 
   return (
     <Container maxW={"container.xl"}>
-      <SimpleGrid columns={2}>{projects}</SimpleGrid>
+      <SimpleGrid
+        columns={2}
+        // columns={{ sm: 2, md: 3 }}
+        spacing={10}
+      >
+        {projects}
+      </SimpleGrid>
+      <ProjectModal project={modalContent} setModalContent={setModalContent} />
     </Container>
   )
 }
