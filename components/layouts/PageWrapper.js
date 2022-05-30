b199847e95b7e7f610973fa5bd287544d40d5f42
Patryk <byszek.patryk@gmail.com>
2022-05-30 23:37:02 +0200

    Small responsiveness fix

--- components/layouts/PageWrapper.js
+++ components/layouts/PageWrapper.js
@@ -1,10 +1,25 @@
 import { Container } from "@chakra-ui/react"
+import { motion } from "framer-motion"
 
 const PageWrapper = ({ children, ...props }) => {
+  const variants = {
+    hidden: { opacity: 0, x: 0, y: 20 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: 20 },
+  }
+
   return (
-    <Container maxW={"100%"} pt={120} {...props}>
-      {children}
-    </Container>
+    <motion.article
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+      exit="exit"
+      transition={{ duration: 0.5, type: "easeInOut" }}
+    >
+      <Container maxW={"100%"} pt={120} {...props}>
+        {children}
+      </Container>
+    </motion.article>
   )
 }
 
