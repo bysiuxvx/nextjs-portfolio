7f4779a0d6e1d05e41128b2b275ed47b5dafc4f8
Patryk <byszek.patryk@gmail.com>
2022-06-01 21:05:35 +0200

    Updated description

--- data/project-list.js
+++ data/project-list.js
@@ -45,7 +45,7 @@ export const projectList = [
     description: `I wanted to update my previous portfolio I made around May 2021, but instead I chose to build a new one from scratch,
     while reusing some concepts of the previous one.
     First and foremost, I decided to build it in Next.js this time around, since I really got to like this framework recently. 
-    Secondly, I decided to use a UI library instead of writing literally all the styling and media queries on my own.
+    Secondly, I decided to use a UI library instead of writing literally all the styling and media queries on my own as with the first one.
     This is a huge improvement already compared to the previous one, and the experience is much smoother.
     Also Chakra UI allowed me to effortlelsy use and switch between themes.`,
     shortDescription: "The page you have in front of you 🤓.",
@@ -53,7 +53,14 @@ export const projectList = [
     imageAlt: "Dog model",
     githubURL: "https://github.com/bysiuxvx/nextjs-homepage",
     liveDemo: "https://pbyszek.com",
-    techStack: ["Next.js", "Chakra UI", "React Three Fiber", "Emotion"],
+    techStack: [
+      "Next.js",
+      "Chakra UI",
+      "React Three Fiber",
+      "Emotion",
+      "Express",
+      "Nodemailer",
+    ],
   },
   {
     name: "React Typescript Bookstore",
