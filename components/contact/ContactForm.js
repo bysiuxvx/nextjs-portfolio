0db70ac0a912ed9f6df94d4e1ec247b9f73ad340
Patryk <byszek.patryk@gmail.com>
2022-10-18 02:59:23 +0200

    code refactor, layout changes

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -6,73 +6,83 @@ import axios from "axios";
 import TextField from "./TextField";
 
 import { Box, Button, FormLabel, useToast } from "@chakra-ui/react";
+import Greeter from "../greeter";
+import PageWrapper from "../layouts/PageWrapper";
 
 const ContactForm = () => {
   const toast = useToast();
 
   return (
-    <Formik
-      initialValues={{ name: "", email: "", message: "" }}
-      validationSchema={yupValidation}
-      onSubmit={(values, { setSubmitting, resetForm }) => {
-        setSubmitting(true);
+    <PageWrapper mt={-100}>
+      <Greeter>
+        If you are interested in some sort of collaboration you can reach me
+        through here!
+      </Greeter>
+      <Formik
+        initialValues={{ name: "", email: "", message: "" }}
+        validationSchema={yupValidation}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
 
-        axios
-          .post("/api/contact", {
-            name: values.name,
-            email: values.email,
-            message: values.message,
-          })
-          .then((res) => {
-            if (!res.data.success) {
-              toast(toastOptions.failed);
-              setSubmitting(false);
-            } else {
-              toast(toastOptions.success);
-              setSubmitting(false);
-              resetForm();
-            }
-          });
-      }}
-    >
-      {(formik) => (
-        <Box as="form" onSubmit={formik.handleSubmit} pb={"100px"}>
-          <TextField
-            name="name"
-            type="name"
-            component="input"
-            label="Your name"
-            placeholder="Enter your name"
-            disabled={formik.isSubmitting}
-          />
-          <TextField
-            name="email"
-            type="email"
-            component="input"
-            label="Your email"
-            placeholder="Enter your email"
-            disabled={formik.isSubmitting}
-          />
-          <TextField
-            name="message"
-            type="message"
-            component="textarea"
-            label="Your message"
-            placeholder="Enter your message"
-            disabled={formik.isSubmitting}
-          />
-          {formik.isSubmitting && <FormLabel pt={2}>Please wait...</FormLabel>}
-          <Button
-            type="submit"
-            disabled={formik.isSubmitting}
-            isLoading={formik.isSubmitting}
-            mt={5}
-          >
-            Submit
-          </Button>
-        </Box>
-      )}
-    </Formik>
+          axios
+            .post("/api/contact", {
+              name: values.name,
+              email: values.email,
+              message: values.message,
+            })
+            .then((res) => {
+              if (!res.data.success) {
+                toast(toastOptions.failed);
+                setSubmitting(false);
+              } else {
+                toast(toastOptions.success);
+                setSubmitting(false);
+                resetForm();
+              }
+            });
+        }}
+      >
+        {(formik) => (
+          <Box as="form" onSubmit={formik.handleSubmit} pb={"100px"}>
+            <TextField
+              name="name"
+              type="name"
+              component="input"
+              label="Your name"
+              placeholder="Enter your name"
+              disabled={formik.isSubmitting}
+            />
+            <TextField
+              name="email"
+              type="email"
+              component="input"
+              label="Your email"
+              placeholder="Enter your email"
+              disabled={formik.isSubmitting}
+            />
+            <TextField
+              name="message"
+              type="message"
+              component="textarea"
+              label="Your message"
+              placeholder="Enter your message"
+              disabled={formik.isSubmitting}
+            />
+            {formik.isSubmitting && (
+              <FormLabel pt={2}>Please wait...</FormLabel>
+            )}
+            <Button
+              type="submit"
+              disabled={formik.isSubmitting}
+              isLoading={formik.isSubmitting}
+              mt={5}
+            >
+              Submit
+            </Button>
+          </Box>
+        )}
+      </Formik>
+    </PageWrapper>
   );
 };
 
