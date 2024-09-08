eeca565f49b84c39e7ed7a814aa9e50ae570711b
Patryk B <byszek.patryk@gmail.com>
2024-09-08 14:34:33 +0200

    refactor of the tech list

--- components/about/TechnologiesAccordion.js
+++ components/about/TechnologiesAccordion.js
@@ -9,7 +9,7 @@ import {
   Text,
 } from "@chakra-ui/react"
 
-import { SettingsIcon, ViewIcon } from "@chakra-ui/icons"
+import { EditIcon, SettingsIcon, ViewIcon } from "@chakra-ui/icons"
 
 const TechnologiesAccordion = () => {
   return (
@@ -24,39 +24,21 @@ const TechnologiesAccordion = () => {
           </AccordionButton>
         </Text>
         <AccordionPanel>
-          <AccordionItem>
-            <Text>
-              <AccordionButton _expanded={{ bg: "#e2d6bf80" }}>
-                <Box flex="1" textAlign="left">
-                  <Icon as={ViewIcon} /> Chakra UI
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </Text>
-            <AccordionPanel textAlign={"justify"}>
-              Honestly, by far the best and most fun UI library I ever used. So
-              far I&apos;ve used it several times, and this portfolio was also
-              built with it. I love how easy it is to set up themes and the
-              theme switching component itselft, which can be used for example
-              for light & dark modes. Can only recommend.
-            </AccordionPanel>
-          </AccordionItem>
-          <AccordionItem>
-            <Text>
-              <AccordionButton _expanded={{ bg: "#e2d6bf80" }}>
-                <Box flex="1" textAlign="left">
-                  <Icon as={SettingsIcon} /> Zustand
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </Text>
-            <AccordionPanel textAlign={"justify"}>
-              Simple and easy to use state management that kind of reminds me of
-              Redux Toolkit, but with even less boilerplate. I have used it
-              already on several occassions and it&apos;s my go to solution
-              whenever I have to work with state.
-            </AccordionPanel>
-          </AccordionItem>
+          {techElements.map((element) => (
+            <AccordionItem key={element.name}>
+              <Text>
+                <AccordionButton _expanded={{ bg: "#e2d6bf80" }}>
+                  <Box flex="1" textAlign="left">
+                    <Icon as={element.icon} /> {element.name}
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </Text>
+              <AccordionPanel textAlign={"justify"}>
+                {element.description}
+              </AccordionPanel>
+            </AccordionItem>
+          ))}
         </AccordionPanel>
       </AccordionItem>
     </Accordion>
@@ -64,3 +46,36 @@ const TechnologiesAccordion = () => {
 }
 
 export default TechnologiesAccordion
+
+const techElements = [
+  {
+    name: "Chakra UI",
+    icon: ViewIcon,
+    description:
+      "I appreciate this library for it's simplicity. So far I&apos;ve used it several times, and this portfolio was also built with it.",
+  },
+  {
+    name: "shadcn/ui",
+    icon: ViewIcon,
+    description:
+      "Currently I don't have a public project using this library. As of now (september 2024) I am working on a bigger project that uses shadcn/ui with Tailwind. It's the perfect library to integrate with Next.js.",
+  },
+  {
+    name: "Jotai",
+    icon: SettingsIcon,
+    description:
+      "Simple and easy to use state management. It's from the same developer who made Zustand, another library I've enjoyed and used in the past. Jotai is even simpler than Zustand. Just create an atom in a store file, and import it wherever you feel the need to. It's as simple as that.",
+  },
+  {
+    name: "SWR / React Query",
+    icon: SettingsIcon,
+    description:
+      "Both libraries are perfect for handling and invalidating data. Simplicity and performance, allows for quick development of applications.",
+  },
+  {
+    name: "Prisma",
+    icon: EditIcon,
+    description:
+      "ORM for simplifying database queries. Great developer experience, efficient on top of it.",
+  },
+]
